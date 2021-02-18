279605d1e4edf25625b588b4a9487b333aa1a4e6
Paul Oms <support@ohmysmtp.com>
2021-02-18 18:23:00 +0000

    Removed 2MB limit on email size

--- src/lib/DomainClient.spec.ts
+++ src/lib/DomainClient.spec.ts
@@ -14,7 +14,7 @@ const defaultRequestHeaders = {
   Accept: 'application/json',
   'Content-Type': 'application/json',
   'OhMySMTP-Server-Token': token,
-  'User-Agent': 'ohmysmtp.js - 0.0.5',
+  'User-Agent': 'ohmysmtp.js - 0.0.6',
 };
 
 test('can initialize the domain client with default values', (t) => {
